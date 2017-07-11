47f7e8bd766fa51b9ef86bd8d19fa6e2e3a0ad47
Henry Addo <henry@addhen.org>
2017-07-11 20:56:31 +0200

    Setup script for building a docker image for android ci

--- build.sh
+++ build.sh
@@ -5,16 +5,17 @@ set -e
 # This dir
 SETUP_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SETUP_DIR
-
 LINK_TO_LATEST=${LINK_TO_LATEST-0}
 # Current Version
 version="$(cat files/VERSION)"
 # We use quay
-repo_name="$(cat files/REPO_NAME)"
-
+repo_name="$(cat files/REPO-NAME)"
 DOCKER_VERSION="${repo_name}:${version}"
 DOCKER_LATEST="${repo_name}:latest"
-
+major=( ${version//./ } )
+echo "******* UPDATING COMPONENTS VERSION to ${a[0]} and $version *******"
+sed -i '' -e "s|\-[0-9].|-${major[0]}|" files/android-components-versions.sh
+sed -i '' -e "s|[0-9]*\.[0-9]*\.[0-9]*|$version|" files/android-components-versions.sh
 echo "******* BUILDING *******"
 docker build -t ${DOCKER_VERSION} -t ${DOCKER_LATEST} -f Dockerfile .
 echo ""
