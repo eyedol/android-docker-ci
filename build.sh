1fdf02395b5e0c9ce840d0d032295104e44a4bec
Henry Addo <henry@addhen.org>
2017-07-12 14:24:06 +0200

    Minor code clean

--- build.sh
+++ build.sh
@@ -12,17 +12,10 @@ version="$(cat files/VERSION)"
 repo_name="$(cat files/REPO-NAME)"
 DOCKER_VERSION="${repo_name}:${version}"
 DOCKER_LATEST="${repo_name}:latest"
-#major=( ${version//./ } )
-#echo "******* UPDATING COMPONENTS VERSION to ${a[0]} and $version *******"
-#sed -i '' -e "s|\-[0-9].|-${major[0]}|" files/android-components-versions.sh
-#sed -i '' -e "s|[0-9]*\.[0-9]*\.[0-9]*|$version|" files/android-components-versions.sh
 echo "******* BUILDING *******"
 docker build -t ${DOCKER_VERSION} -t ${DOCKER_LATEST} -f Dockerfile .
-echo ""
-
 echo "******* PUSHING ${DOCKER_VERSION} *******"
 docker push ${DOCKER_VERSION}
-
 if [ "${LINK_TO_LATEST}" == "0" ]; then
     echo "Not linking to latest"
 else
